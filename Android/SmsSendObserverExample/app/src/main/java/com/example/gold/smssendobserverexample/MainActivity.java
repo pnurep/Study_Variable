8412c998fbe3890a0333cc21d40d790e9b70776d
금 <pnurep@gmail.com>
2017-12-29 17:04:09 +0900

    '17.12.29. telephony 추가

--- Android/SmsSendObserverExample/app/src/main/java/com/example/gold/smssendobserverexample/MainActivity.java
+++ Android/SmsSendObserverExample/app/src/main/java/com/example/gold/smssendobserverexample/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.gold.smssendobserverexample;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,7 +20,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements SmsSendObserver.SmsSendListener {
 
-    Button button;
+    Button button, btn_sim;
     EditText et_pNum, et_content;
 
     @Override
@@ -26,6 +29,7 @@ public class MainActivity extends AppCompatActivity implements SmsSendObserver.S
         setContentView(R.layout.activity_main);
 
         button = (Button) findViewById(R.id.button);
+        btn_sim = (Button) findViewById(R.id.btn_sim);
         et_pNum = (EditText) findViewById(R.id.pNum);
         et_content = (EditText) findViewById(R.id.content);
 
@@ -46,16 +50,67 @@ public class MainActivity extends AppCompatActivity implements SmsSendObserver.S
 
                 TedPermission.with(MainActivity.this).setPermissionListener(permissionlistener).
                         setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]").
-                        setPermissions(Manifest.permission.READ_CONTACTS, Manifest.permission.ACCESS_FINE_LOCATION).check();
+                        setPermissions(Manifest.permission.READ_SMS, Manifest.permission.RECEIVE_SMS, Manifest.permission.SEND_SMS).check();
 
             }
         });
+
+        btn_sim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                PermissionListener permissionlistener = new PermissionListener() {
+//                    @Override
+//                    public void onPermissionGranted() {
+//                        sendMessage(et_pNum.getText().toString(), et_content.getText().toString());
+//                    }
+//
+//                    @Override
+//                    public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+//                        Toast.makeText(MainActivity.this, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+//                    }
+//                };
+//
+//                TedPermission.with(MainActivity.this).setPermissionListener(permissionlistener).
+//                        setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]").
+//                        setPermissions(Manifest.permission.READ_PHONE_STATE).check();
+
+                //권한필요없네??
+                TelephonyManager telMgr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+                getTelephonyInfo(telMgr);
+            }
+        });
+
+    }
+
+    private boolean getTelephonyInfo(TelephonyManager telMgr) {
+        int simStatus = telMgr.getSimState();
+        switch (simStatus) {
+            case TelephonyManager.SIM_STATE_ABSENT:
+                return false;
+            case TelephonyManager.SIM_STATE_NETWORK_LOCKED:
+                return true;
+            case TelephonyManager.SIM_STATE_PIN_REQUIRED:
+                return true;
+            case TelephonyManager.SIM_STATE_PUK_REQUIRED:
+                return true;
+            case TelephonyManager.SIM_STATE_READY:
+                return true;
+            case TelephonyManager.SIM_STATE_UNKNOWN:
+                return false;
+            default:
+                return false;
+        }
     }
 
     @Override
     public void onSmsSendEvent(boolean sent) {
-        if (sent)
-            Toast.makeText(this, sent ? "Message was sent" : "Timed out", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, sent ? "Message was sent" : "Timed out", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("onResume", "===========================");
     }
 
     public void sendMessage(String phoneNumber, String messageBody) {
@@ -69,4 +124,8 @@ public class MainActivity extends AppCompatActivity implements SmsSendObserver.S
         intent.putExtra("exit_on_sent", true);
         startActivity(intent);
     }
+
+
 }
+
+
